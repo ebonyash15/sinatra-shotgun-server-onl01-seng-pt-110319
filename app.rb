4f6da4b3cf5443be4ca115734987b579f193c629
Ebony Sharrock <ebonysharrock@gmail.com>
2020-02-10 12:17:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! I built this using shotguns!"
+    "Welcome to your app!!!! I built this using shotgun!"
   end
 
 end
